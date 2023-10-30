ba1adc47799205488e080481ee513ee084adfbf2
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2023-10-30 14:29:08 +0300

    change ascii numbering

--- src/ascii.rs
+++ src/ascii.rs
@@ -22,7 +22,7 @@ fn draw_board(
     let mut values = String::new();
     let mut bot_jags = String::new();
 
-    let mut col_idxs = String::new();
+    let mut file_idxs = String::new();
 
     for (rank, row) in &rows {
         let radius = radius.get_or_init(|| rank.abs());
@@ -30,29 +30,19 @@ fn draw_board(
         let j_o = 7 + v_o;
 
         write!(top_jags, "{:>j_o$}", "")?;
-        write!(values, "{:>v_o$}{:>5} ", "", rank.to_string().blue())?;
+        write!(
+            values,
+            "{:>v_o$}{:>5} ",
+            "",
+            (radius - rank + 1).to_string().blue()
+        )?;
         write!(bot_jags, "{:>j_o$}", "")?;
 
-        for h in row {
-            let is_corner = ((h.x.abs() == *radius || h.x == 0)
-                && (h.y.abs() == *radius || h.y == 0))
-                ^ (h.x == 0 && h.y == 0);
-
-            let is_edge =
-                (h.x.abs() == *radius) ^ (h.y.abs() == *radius) ^ (h.z().abs() == *radius);
-
-            let fill = if is_edge {
-                "E"
-            } else if is_corner {
-                "C"
-            } else {
-                ""
-            };
-
+        for _ in row {
             if rank <= 0 {
                 write!(top_jags, "{} \\ ", "/".purple())?;
             }
-            write!(values, "{}{:>2} ", "|".blue(), fill)?;
+            write!(values, "{}{:>2} ", "|".blue(), "")?;
             if rank >= 0 {
                 write!(bot_jags, "\\ {} ", "/".purple())?;
             }
@@ -60,13 +50,15 @@ fn draw_board(
 
         write!(values, "{}", "|".blue())?;
         if rank > 0 {
-            write!(values, " {}", (*radius + 1 - rank).to_string().purple())?;
+            let file = (b'a' as i32 + 2 * radius - rank + 1) as u8 as char;
+            write!(values, " {}", file.to_string().purple())?;
         } else {
-            write!(col_idxs, "{:>4}", rank)?;
+            let file = (rank + radius + b'a' as i32) as u8 as char;
+            write!(file_idxs, "{:>4}", file)?;
         }
 
         if rank == *radius {
-            writeln!(bot_jags, "\n{:>j_o$}{}", "", col_idxs.purple())?;
+            writeln!(bot_jags, "\n{:>j_o$}{}", "", file_idxs.purple())?;
         }
 
         if rank <= 0 {
