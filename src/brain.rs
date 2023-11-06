8763c8fde0b3610713e07d9bce435bcb32862a12
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2023-11-07 01:13:30 +0300

    removing buggy undo and other things

--- src/brain.rs
+++ src/brain.rs
@@ -1,11 +1,12 @@
-use crate::board::Board;
+use crate::{board::Board, game_data::GameState};
+use hexx::Hex;
 use minimax::*;
 
 struct Game;
 struct Move(i32, i32);
 
 impl minimax::Game for Game {
-    type S = Board<7>;
+    type S = Board<9>;
     type M = hexx::Hex;
 
     fn generate_moves(state: &Self::S, moves: &mut Vec<Self::M>) {
@@ -18,13 +19,9 @@ impl minimax::Game for Game {
     }
 
     fn apply(state: &mut Self::S, m: Self::M) -> Option<Self::S> {
+        let mut state = state.clone();
         state.move_at(m).ok();
-        None
-    }
-
-    fn undo(state: &mut Self::S, m: Self::M) {
-        let mut foo = state.state[&m];
-        foo = None;
+        Some(state)
     }
 
     fn get_winner(state: &Self::S) -> Option<Winner> {
@@ -36,31 +33,58 @@ impl minimax::Game for Game {
     }
 }
 
+pub fn run_() {
+    let mut state = Board::new();
+    perft::<Game>(&mut state, 100, true);
+
+    //  println!("{foo:?}");
+}
+
 pub fn run() {
     use minimax::strategies::negamax::Negamax;
     use minimax::{Game, Strategy};
+    let mut gs = GameState::Ongoing;
+    while !matches!(gs, GameState::Draw) {
+        let mut b = Board::new();
+        let mut s = 0;
+        while self::Game::get_winner(&b).is_none() {
+            // println!("{}", b);
+            if s == 0 {
+                // let mut strategy = MonteCarloTreeSearch::<crate::brain::Game>::new(
+                //     MCTSOptions::default().with_rollouts_before_expanding(5),
+                // );
+                // strategy.set_timeout(Duration::from_secs(1));
+                let mut strategy = Random::<crate::brain::Game>::new();
 
-    let mut b = Board::new();
-
-    let mut s = 0;
-    while self::Game::get_winner(&b).is_none() {
-        println!("{}", b);
-        if s == 0 {
-            let mut strategy =
-                MonteCarloTreeSearch::<crate::brain::Game>::new(MCTSOptions::default());
-
-            match strategy.choose_move(&mut b) {
-                Some(m) => self::Game::apply(&mut b, m),
-                None => break,
+                match strategy.choose_move(&mut b) {
+                    Some(m) => {
+                        // println!("{m:?}");
+                        b = self::Game::apply(&mut b, m).unwrap();
+                    }
+                    None => break,
+                };
+            } else {
+                // let mut strategy = MonteCarloTreeSearch::<crate::brain::Game>::new(
+                //     MCTSOptions::default().with_rollouts_before_expanding(5),
+                // );
+                // strategy.set_timeout(Duration::from_secs(1));
+                let mut strategy = Random::<crate::brain::Game>::new();
+                match strategy.choose_move(&mut b) {
+                    Some(m) => {
+                        // println!("{m:?}");
+                        b = self::Game::apply(&mut b, m).unwrap();
+                    }
+                    None => break,
+                };
             };
-        } else {
-            let mut strategy = Random::<crate::brain::Game>::new();
-            match strategy.choose_move(&mut b) {
-                Some(m) => self::Game::apply(&mut b, m),
-                None => break,
-            };
-        };
-        s = 1 - s;
+            s = 1 - s;
+            // thread::sleep(Duration::from_secs(1));
+        }
+
+        let state = b.get_game_state();
+
+        println!("{state:?}");
+        // println!("{b}");
+        gs = state;
     }
-    println!("{}", b);
 }
